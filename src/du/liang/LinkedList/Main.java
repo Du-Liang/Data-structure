6d1f666666a848952b3dc3392e9e1a02222cf991
ll <784008359@qq.com>
2020-01-13 20:54:33 +0800

    单项循环链表add完成，该写remove方法。2020年1月13日 20:54:26

--- src/du/liang/LinkedList/Main.java
+++ src/du/liang/LinkedList/Main.java
@@ -1,24 +1,20 @@
 package du.liang.LinkedList;
 
-import du.liang.LinkedList.singlelist.SingleLinkedList;
+
+import du.liang.LinkedList.singlelist.CircleSingleLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list=new LinkedList<>();
+        List<Integer> list=new CircleSingleLinkedList<>();
 
-//        for (int i = 0; i < 10; i++) {
-//            list.add(i);
-//        }
-//        list.add(5,666);
-//        System.out.println(list);
-//        list.remove(9);
-//        System.out.println(list);
+        java.util.LinkedList<Integer> ty;
+
         list.add(20);
-//        list.add(0,10);
+
         list.add(30);
         list.add(list.size(),40);
         System.out.println(list);
-        list.remove(2);
+        list.add(0,999);
         System.out.println(list);
     }
 }
---
+++ src/du/liang/LinkedList/singlelist/CircleSingleLinkedList.java
@@ -0,0 +1,220 @@
+package du.liang.LinkedList.singlelist;
+
+import du.liang.LinkedList.AbstractList;
+
+public class CircleSingleLinkedList<E>  extends AbstractList<E> {
+
+    private Node firstNode;
+    private static class Node<E>{
+        E elementE;
+        Node next;
+
+        public Node(E elementE, Node next) {
+            this.elementE = elementE;
+            this.next = next;
+
+        }
+    }
+
+    /**
+     * 清除所有元素
+     */
+    @Override
+    public void clear() {
+        size=0;
+        firstNode = null;
+
+    }
+
+    /**
+     * 元素的数量
+     *
+     * @return
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 是否为空
+     *
+     * @return
+     */
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    /**
+     * 是否包含某个元素
+     *
+     * @param element
+     * @return
+     */
+    @Override
+    public boolean contains(Object element) {
+        return indexOf(element)!=ELEMENT_NOT_FOUND;
+    }
+
+    /**
+     * 添加元素到尾部
+     *
+     * @param element
+     */
+    @Override
+    public void add(E element) {
+
+        add(size,element);
+
+    }
+
+    /**
+     * 获取index位置的元素
+     *
+     * @param index
+     * @return
+     */
+    @Override
+    public E get(int index) {
+        return findNode(index).elementE;
+    }
+
+    /**
+     * 设置index位置的元素
+     *
+     * @param index
+     * @param element
+     * @return 原来的元素ֵ
+     */
+    @Override
+    public E set(int index, E element) {
+        Node<E> old,node=findNode(index);
+        old=node;
+        node.elementE=element;
+        return old.elementE;
+    }
+
+    /**
+     * 在index位置插入一个元素
+     *
+     * @param index
+     * @param element
+     */
+    @Override
+    public void add(int index, E element) {
+
+        if (index==0){
+
+            Node<E> node= new Node<E>(element,firstNode);
+            firstNode=node;
+            Node<E> LastNode=(size==0)?firstNode:findNode(size-1);
+            LastNode.next=firstNode;
+//            firstNode.next=firstNode;
+
+        }else {
+
+            Node<E> previous= findNode(index-1);
+
+
+            Node<E> newNode=new Node<E>(element,previous.next);
+            previous.next=newNode;
+//            newNode.next=firstNode;
+
+        }
+
+
+        size++;
+    }
+
+    //找到index位置所对应的节点
+    private Node<E> findNode(int index){
+        rangeCheck(index);
+        Node presentNode=firstNode;
+        int n=0;
+        while (n<index){
+            if(presentNode.next!=null){
+                presentNode=presentNode.next;
+            }
+            n++;
+        }
+        return presentNode;
+    }
+
+    /**
+     * 删除index位置的元素
+     *
+     * @param index
+     * @return
+     */
+    @Override
+    public E remove(int index) {
+        Node<E> old=firstNode;
+        if(index==0){
+
+            firstNode=firstNode.next;
+        }else {
+            Node prev=findNode(index-1);
+            old =prev.next;
+            prev.next=prev.next.next;
+
+        }
+        size--;
+        return old.elementE;
+    }
+
+    /**
+     * 查看元素的索引
+     *
+     * @param element
+     * @return
+     */
+    @Override
+    public int indexOf(Object element) {
+        int indexOfElement=ELEMENT_NOT_FOUND;
+        int i=0;
+        Node<E> node=firstNode;
+        if(element==null){
+            while (node!=null){
+                if(node.elementE==null){
+                    indexOfElement=i;
+                    break;
+                }
+                i++;
+                node=node.next;
+            }
+        }else {
+            while (node!=null){
+                if(node.elementE.equals(element)){
+                    indexOfElement=i;
+                    break;
+                }
+                i++;
+                node=node.next;
+            }
+        }
+
+        return indexOfElement;
+
+    }
+    @Override
+    public String toString() {
+        StringBuilder str=new StringBuilder();
+        str.append("size="+size+" [");
+        Node<E> node=firstNode;
+
+        for (int i = 0; i < size; i++) {
+            if(i!=0){
+                str.append(",");
+            }
+            str.append(node.elementE);
+//            str.append(" next=");
+//            str.append(node.next);
+            node=node.next;
+
+        }
+        str.append("]");
+        return str.toString() ;
+    }
+
+}
